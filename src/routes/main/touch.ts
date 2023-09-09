abd8d206aa36cbcda11a1d5cdd5bc6ab049b18b8
chelizichen <1347290221@qq.com>
2023-09-09 23:37:18 +0800

    feat:文件上传功能优化

--- src/routes/main/touch.ts
+++ src/routes/main/touch.ts
@@ -19,30 +19,42 @@ const opts: RouteShorthandOptions = {
                         type:"number"
                     },
                     data:{
-                        type:"object"
+                        type:"object",
+                        properties:{
+                            uploaded:{
+                                type:"boolean"
+                            },
+                            write_file_path:{
+                                type:"string"
+                            },
+                        }
                     }
                 }
             }
         },
-        body: {
-            userId:{
-                type:"string"
-            }
-        }
+        // body: {
+        //     dir:{
+        //         type:"string"
+        //     },
+
+        // }
     }
 }
-
-const handleFunc: RouteHandlerMethod = async (request: FastifyRequest, reply: FastifyReply) => {
-    console.log(3)
-    return {test: '11it worked!'}
-    // const {userId} = await request.body
-    // const userDir = userId;
-    // const data = await request.file();
-    // const fileContent = await data.toBuffer();
-    // const originalFilename = data.filename;
-    // const file_path = path.resolve(routes,userDir, originalFilename)
-    // fs.writeFileSync(file_path, fileContent)
-    // return Reply(ReplyBody.success,ReplyBody.success_message,null)
+type CustomRequest = FastifyRequest<{
+    Querystring: { dir: string };
+}>
+const handleFunc: RouteHandlerMethod = async (request: CustomRequest, reply: FastifyReply) => {
+    
+    const { dir } = request.query
+    const data = await request.file();
+    console.log('req.file',data);
+    console.log('req.dir',dir);
+    const fileContent = await data.toBuffer();
+    const originalFilename = data.filename;
+    const file_path = path.join(routes,"api",dir,originalFilename)
+    console.log(file_path)
+    fs.writeFileSync(file_path,fileContent)
+    return Reply(ReplyBody.success,ReplyBody.success_message,{ uploaded: true,write_file_path:file_path });
 }
 
 export default function () {
