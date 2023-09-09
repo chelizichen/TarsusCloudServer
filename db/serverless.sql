9f3c5464e95a831754e1e3cb00f3f1c1ca468c9e
chelizichen <1347290221@qq.com>
2023-09-09 22:14:41 +0800

    update:sql

--- db/serverless.sql
+++ db/serverless.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80033 (8.0.33)
  File Encoding         : 65001
 
- Date: 07/09/2023 01:09:27
+ Date: 09/09/2023 22:14:25
 */
 
 SET NAMES utf8mb4;
@@ -24,7 +24,6 @@ DROP TABLE IF EXISTS `dirs`;
 CREATE TABLE `dirs` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` varchar(255) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
   `port` varchar(255) DEFAULT NULL,
   `dir` varchar(255) DEFAULT NULL,
   `pid` int DEFAULT NULL,
@@ -33,15 +32,41 @@ CREATE TABLE `dirs` (
   `is_primary` varchar(255) DEFAULT NULL,
   `primary_id` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of dirs
 -- ----------------------------
 BEGIN;
-INSERT INTO `dirs` (`id`, `user_id`, `user_name`, `port`, `dir`, `pid`, `update_time`, `create_time`, `is_primary`, `primary_id`) VALUES (1, '0', 'admin', '3401', '', 15902, '2023-09-06 21:18:39', '2023-09-06 21:18:41', '1', NULL);
-INSERT INTO `dirs` (`id`, `user_id`, `user_name`, `port`, `dir`, `pid`, `update_time`, `create_time`, `is_primary`, `primary_id`) VALUES (2, '1', '李云', '3411', 'leemulus', 17668, '2023-09-07 00:10:50', '2023-09-07 00:10:52', '0', 1);
-INSERT INTO `dirs` (`id`, `user_id`, `user_name`, `port`, `dir`, `pid`, `update_time`, `create_time`, `is_primary`, `primary_id`) VALUES (3, '2', '李成', '3412', 'chelizichen', 15902, '2023-09-07 00:10:50', '2023-09-07 00:10:52', '0', 1);
+INSERT INTO `dirs` (`id`, `user_id`, `port`, `dir`, `pid`, `update_time`, `create_time`, `is_primary`, `primary_id`) VALUES (1, '1', '3401', '', 15902, '2023-09-06 21:18:39', '2023-09-06 21:18:41', '1', NULL);
+INSERT INTO `dirs` (`id`, `user_id`, `port`, `dir`, `pid`, `update_time`, `create_time`, `is_primary`, `primary_id`) VALUES (2, '2', '3411', 'leemulus', 89210, '2023-09-07 00:10:50', '2023-09-07 00:10:52', '0', 1);
+INSERT INTO `dirs` (`id`, `user_id`, `port`, `dir`, `pid`, `update_time`, `create_time`, `is_primary`, `primary_id`) VALUES (3, '3', '3412', 'chelizichen', 89404, '2023-09-07 00:10:50', '2023-09-07 00:10:52', '0', 1);
+INSERT INTO `dirs` (`id`, `user_id`, `port`, `dir`, `pid`, `update_time`, `create_time`, `is_primary`, `primary_id`) VALUES (4, '2', '3413', 'test', 89304, NULL, NULL, NULL, 1);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `level` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` (`id`, `user_name`, `password`, `email`, `phone`, `level`, `create_time`, `update_time`) VALUES (1, 'admin', '123456', NULL, NULL, '999', NULL, NULL);
+INSERT INTO `users` (`id`, `user_name`, `password`, `email`, `phone`, `level`, `create_time`, `update_time`) VALUES (2, 'leeyun', '123456', NULL, NULL, '1', NULL, NULL);
+INSERT INTO `users` (`id`, `user_name`, `password`, `email`, `phone`, `level`, `create_time`, `update_time`) VALUES (3, 'leechen', '123456', NULL, NULL, '1', NULL, NULL);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
