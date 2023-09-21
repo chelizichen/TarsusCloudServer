f5ab3bd0c13fcf8509ff63529917b5ccadaaae5a
chelizichen <1347290221@qq.com>
2023-09-21 10:07:30 +0800

    feat:结构体优化

--- src/routes/main/taro/touch.ts
+++ src/routes/main/taro/touch.ts
@@ -2,6 +2,7 @@ import {FastifyReply, FastifyRequest, RouteShorthandOptions} from "fastify";
 import path from "path";
 import { Reply, ReplyBody} from "../../../main_control/define";
 import { existsSync, unlinkSync, writeFileSync } from "fs";
+import load_schema from "../../../main_control/schema";
 
 
 const routes = process.env.routes_path;
@@ -9,23 +10,9 @@ const routes = process.env.routes_path;
 const opts: RouteShorthandOptions = {
     schema: {
         response: {
-            200: {
-                type: 'object',
-                properties: {
-                    message: {
-                        type: 'string'
-                    },
-                    code: {
-                        type: "number"
-                    },
-                    data: {}
-                }
-            }
+            200: load_schema.get("TouchTaroFileRsp")
         },
-        body: {
-            dir: {type: "string"},
-            content:{type:"string"}
-        }
+        body: load_schema.get("TaroBaseReq")
     }
 }
 type CustomRequest = FastifyRequest<{
@@ -41,10 +28,10 @@ const handleFunc = async (request: CustomRequest, reply: FastifyReply) => {
     if(existsSync(filePath)){
         unlinkSync(filePath)
     }
-    const fileContent = writeFileSync(filePath,content,{
+    writeFileSync(filePath,content,{
         'encoding':'utf-8'
     })
-    return Reply(ReplyBody.success, ReplyBody.success_message, fileContent);
+    return Reply(ReplyBody.success, ReplyBody.success_message, null);
 }
 
 export default function () {
